94534014c59fde7ac877ea137793680f93593100
MisheruSama <michelgermano010@outlook.com>
2024-08-19 17:09:37 -0300

    Add files via upload

---
+++ Exercício sequencial4.py
@@ -0,0 +1,3 @@
+m = float(input("Digite o valor a ser convertido:"))
+cm = m*100
+print("A medida convertida é de",cm,"centimetros")
---
+++ Exercício sequencial5.py
@@ -0,0 +1,4 @@
+# Sistema que transforma uma medida em metros para centimetros
+m = float(input("Digite o valor a ser convertido:"))
+cm = m*100
+print("A medida convertida é de",cm,"centimetros")
