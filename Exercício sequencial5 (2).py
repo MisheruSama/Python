bdaa1dc637bbbab96d7fe786bcb808485a62fb1a
MisheruSama <michelgermano010@outlook.com>
2024-08-19 17:31:15 -0300

    Add files via upload

---
+++ Exercício sequencial5 (2).py
@@ -0,0 +1,4 @@
+# Sistema que recebe um valor em metros e então o converte em centímetros
+m = float(input("Digite o valor em metros a ser convertido:"))
+cm = m*100
+print("O valor convertido é:",cm,"centímetros")
