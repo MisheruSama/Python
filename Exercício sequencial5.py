b25d10a52908547eea09cbf50c264274918bff57
MisheruSama <michelgermano010@outlook.com>
2024-08-19 17:30:50 -0300

    Delete Exercício sequencial5.py

--- Exercício sequencial5.py
+++
@@ -1,5 +0,0 @@
-# Sistema que recebe dois numeros interios então mostra a soma deles
-x = int(input("Digite o primeiro numero:")) 
-y = int(input("Digite o segundo numero:"))
-soma = x+y
-print("O resultado da soma entre eles é:",soma)
