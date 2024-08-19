a91ab10edf999fd20386fd623ae230479539b5b4
MisheruSama <michelgermano010@outlook.com>
2024-08-19 17:30:58 -0300

    Delete Exercício sequencial6.py

--- Exercício sequencial6.py
+++
@@ -1,4 +0,0 @@
-# Sistema que recebe um valor em metros e então o converte em centímetros
-m = float(input("Digite o valor em metros a ser convertido:"))
-cm = m*100
-print("O valor convertido é:",cm,"centímetros")
