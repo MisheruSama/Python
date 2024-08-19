1fdb5db6d8fa615f40f27bd500882e8988ced360
MisheruSama <michelgermano010@outlook.com>
2024-08-19 17:21:10 -0300

    Update Exercício sequencial2.py

--- Exercício sequencial2.py
+++ Exercício sequencial2.py
@@ -1,3 +1,4 @@
+# Sistema que recebe dois valores e então mostra os valores armazenados
 x = input(r"Digite um numero para x:")
 y = input(r"Digite um numero para y:")
 print(r"O numero escolhido para x foi:",x," enquanto o numero escolhido para y foi:",y)
