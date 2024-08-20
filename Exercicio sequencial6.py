9118e2e4ae5b43c4802334ad273531c69c214b05
MisheruSama <michelgermano010@outlook.com>
2024-08-20 13:14:59 -0300

    Add files via upload

---
+++ Exercicio sequencial6.py
@@ -0,0 +1,5 @@
+# Sistema em que é solicitado o raio de uma circunferência e no fim é dado a área desta circunferência
+r=float(input("Digite o valor do raio da circunferência:"))
+pi=3.14
+A=pi*r**2
+print("A área da circunferência é",A)
