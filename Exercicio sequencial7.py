7aa2c63b4fb86aa22c345b2c58651eb9952095f6
MisheruSama <michelgermano010@outlook.com>
2024-08-20 13:22:10 -0300

    Add files via upload

---
+++ Exercicio sequencial7.py
@@ -0,0 +1,5 @@
+# Sistema que calcula a área de um quadrado e então mostra o dobro dessa área
+b=float(input("Digite a base do quadrado:"))
+h=float(input("Digite a altura do quadrado:"))
+A=(b*h)*2
+print("O dobro da área do quadrado é:",A)
